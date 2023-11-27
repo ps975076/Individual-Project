45b107b86e8d6d4bacf83e6aac4bd7d4b2681820
Priya <Ps975076@gmail.com>
2023-11-27 17:31:19 +0000

    header styling

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,32 +5,47 @@ function App() {
   return (
     <>
       <div className="container">
+        {/* HEADER */}
         <header className="header">
           <div className="menu-container">
             <div className="empty"></div>
             <div className="find">
-              <p>Find a Store | </p>
+              <p>Find a Store &#160; | </p>
             </div>
             <div className="help">
-              <p>Help | </p>
+              <p>Help &#160; | </p>
             </div>
             <div className="join">
-              <p>Join Us | </p>
+              <p>Join Us &#160; | </p>
             </div>
             <div className="signin">
-              <p>Sign In </p>
+              <p>Sign In &#160; &#160; &#160;</p>
             </div>
           </div>
-          <nav className="navBar-container"></nav>
+          <nav className="navBar-container">
+            <div className="logo">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/800px-Logo_NIKE.svg.png"
+                alt="logo"
+              />
+            </div>
+            <div className="navigation"></div>
+            <div className="searchBar">
+              <input className="searchInput" type="text" placeholder="Search" />
+            </div>
+            <div className="favs">
+              <img src="/src/assets/heart-svgrepo-com (3).svg" alt="favs" />
+            </div>
+            <div className="cart">
+              <img src="/src/assets/shopping-bag-svgrepo-com.svg" alt="" />
+            </div>
+          </nav>
         </header>
+        {/* MAIN */}
         <main className="main">main</main>
+        {/* FOOTER */}
         <footer className="footer">footer</footer>
       </div>
-
-      {/* <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/800px-Logo_NIKE.svg.png"
-        alt="logo"
-      /> */}
     </>
   );
 }
