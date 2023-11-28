f9839ed2f6c4506c17a7ee3826c0e2dec3c5c387
Priya <Ps975076@gmail.com>
2023-11-28 15:32:39 +0000

    footer done

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -105,11 +105,30 @@ function App() {
               <p>
                 <strong>HELP &#38; INFORMATION</strong>
               </p>
+              <p>Help</p>
+              <p>Track order</p>
+              <p>Delivery &#38; Returns </p>
+              <p>10% Student Discount</p>
             </div>
-            <div className="about"></div>
-            <div className="contact"></div>
+            <div className="about">
+              <p>
+                <strong>ABOUT US</strong>
+              </p>
+              <p>About Us</p>
+              <p>Careers at Stuff</p>
+              <p>Corporate Responsibility</p>
+              <p>Cyber Security</p>
+            </div>
+            <div className="contact">
+              <p>
+                <strong>CONTACT US</strong>
+              </p>
+              <p>Contact Us</p>
+            </div>
           </div>
-          <div className="rights"></div>
+          <div className="rights">
+            <p> &#169; 2023 Stuff</p>
+          </div>
         </footer>
       </div>
     </>
