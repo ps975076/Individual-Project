2386deb73695f177448199badbf70bf4012fd7dc
Priya <Ps975076@gmail.com>
2023-11-29 12:05:28 +0000

    component split

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,6 @@
 // import { useState } from "react";
+import { useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./Pages/Components/Footer";
 import HeaderNavBar from "./Pages/Components/HeaderNavBar";
@@ -6,13 +8,28 @@ import MainContent from "./Pages/Components/MainContent";
 
 function App() {
   // 1. Create state for products
-  // const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  // 3. useEffect
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // 2. Fetch the product
+  const fetchData = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        setProducts(data);
+      });
+  };
 
   return (
     <>
       <div className="container">
         <HeaderNavBar />
-        <MainContent />
+        <MainContent products={products} />
         <Footer />
       </div>
     </>
--- my-react-app/src/Pages/Components/MainContent.jsx
+++ my-react-app/src/Pages/Components/MainContent.jsx
@@ -1,21 +1,23 @@
-function MainContent() {
+import React from "react";
+
+function MainContent({ products }) {
   return (
     <main className="main">
       <div className="empty-left-col"></div>
       <div className="main-content-col">
         <div className="product">
           <div className="product-img">IMG</div>
-          <div className="product-des">DES</div>
+          <div className="product-des">Title-DES</div>
           <div className="product-price">
             PRICE
             <div className="addToCartBtn">
-              <button>Add to Cart</button>
+              <button>Add to Bag</button>
             </div>
           </div>
         </div>
         <div className="product">
           <div className="product-img">IMG</div>
-          <div className="product-des">DES</div>
+          <div className="product-des"></div>
           <div className="product-price">PRICE</div>
         </div>
         <div className="product">
@@ -48,6 +50,11 @@ function MainContent() {
           <div className="product-des">DES</div>
           <div className="product-price"></div>
         </div>
+        <div className="product">
+          <div className="product-img"></div>
+          <div className="product-des">DES</div>
+          <div className="product-price"></div>
+        </div>
       </div>
       <div className="empty-right-col"></div>
     </main>
@@ -55,3 +62,17 @@ function MainContent() {
 }
 
 export default MainContent;
+
+//   {
+//     products.map((product, index) => (
+//       <div className="product" key={index}>
+//         <div className="product-img">{product.image}</div>
+//         <div className="product-des">{product.title}</div>
+//         <div className="product-price">
+//           <div className="addToCartBtn">
+//             <button>Add to Bag</button>
+//           </div>
+//         </div>
+//       </div>
+//     ));
+//   }
